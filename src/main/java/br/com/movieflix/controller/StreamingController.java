80be0c43bb4937a9b28ef5a8b05c87be1f7e92d5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-06-22 11:41:28 -0300

    feat: criação da classe StreamingController.

---
+++ src/main/java/br/com/movieflix/controller/StreamingController.java
@@ -0,0 +1,15 @@
+package br.com.movieflix.controller;
+
+import br.com.movieflix.service.StreamingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movieflix/streaming")
+@RequiredArgsConstructor
+public class StreamingController {
+
+    private final StreamingService streamingService;
+
+}
